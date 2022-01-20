a2d2f7bf9d7d9ccee99fbc157cff9c89a2ff169b
ChitraNallasamy <91539731+ChitraNallasamy@users.noreply.github.com>
2022-01-20 12:07:13 +0530

    Update ColorCodeRefManual.h

--- ColorCodeRefManual.h
+++ ColorCodeRefManual.h
@@ -1,4 +1,4 @@
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+#define MAX_COLORPAIR_NAME_CHARS = 16;
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
